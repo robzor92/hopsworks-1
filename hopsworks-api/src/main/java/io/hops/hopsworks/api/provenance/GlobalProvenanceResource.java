776a127c12abb1531b95d0f3d1cc5d7f175024ce
Alex Ormenisan <ormenisan.adrian@gmail.com>
2019-04-27 14:55:12 +0200

    provenance use hops 2.8.2.8 snapshot meta enable the ml asset dirs provenance rspec dfso with xattr provenance count and xattr search xattr raw and nested with search and count global provenance endpoint xattr as json add/update/delete

---
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/provenance/GlobalProvenanceResource.java
@@ -0,0 +1,112 @@
+/*
+ * Changes to this file committed after and not including commit-id: ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b
+ * are released under the following license:
+ *
+ * This file is part of Hopsworks
+ * Copyright (C) 2018, Logical Clocks AB. All rights reserved
+ *
+ * Hopsworks is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Hopsworks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Changes to this file committed before and including commit-id: ccc0d2c5f9a5ac661e60e6eaf138de7889928b8b
+ * are released under the following license:
+ *
+ * Copyright (C) 2013 - 2018, Logical Clocks AB and RISE SICS AB. All rights reserved
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS  OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.hops.hopsworks.api.provenance;
+
+import io.hops.hopsworks.api.filter.AllowedProjectRoles;
+import io.hops.hopsworks.api.filter.Audience;
+import io.hops.hopsworks.api.filter.NoCacheResponse;
+import io.hops.hopsworks.common.elastic.ElasticController;
+import io.hops.hopsworks.common.provenance.MLAssetHit;
+import io.hops.hopsworks.common.provenance.Provenance;
+import io.hops.hopsworks.common.provenance.SimpleResult;
+import io.hops.hopsworks.exceptions.GenericException;
+import io.hops.hopsworks.exceptions.ProjectException;
+import io.hops.hopsworks.exceptions.ServiceException;
+import io.hops.hopsworks.jwt.annotation.JWTRequired;
+import io.hops.hopsworks.restutils.RESTCodes;
+import io.swagger.annotations.Api;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Stateless
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Api(value = "Global Provenance Service", description = "Global Provenance Service")
+@Path("/provenance")
+public class GlobalProvenanceResource {
+  private static final Logger logger = Logger.getLogger(ProjectProvenanceResource.class.getName());
+  
+  @EJB
+  private NoCacheResponse noCacheResponse;
+  @EJB
+  private ElasticController elasticController;
+  
+  @GET
+  @Path("mlType/{mlType}/list")
+  @Produces(MediaType.APPLICATION_JSON)
+  @AllowedProjectRoles({AllowedProjectRoles.DATA_SCIENTIST, AllowedProjectRoles.DATA_OWNER})
+  @JWTRequired(acceptedTokens = {Audience.API}, allowedUserRoles = {"HOPS_ADMIN", "HOPS_USER"})
+  public Response getMLAssets(@PathParam("mlType") Provenance.MLType mlType,
+    @BeanParam MLAssetListQueryParamsBean mlAssetsParams,
+    @BeanParam GeneralQueryParamsBean queryParams,
+    @Context HttpServletRequest req) throws ServiceException, GenericException, ProjectException {
+    logger.log(Level.INFO, "Local content path {0} mlassets params:{1} query params:{2}",
+      new Object[]{req.getRequestURL().toString(), mlAssetsParams, queryParams});
+    if (mlType == null) {
+      throw new GenericException(RESTCodes.GenericErrorCode.ILLEGAL_ARGUMENT, Level.INFO, "ml asset type is not set");
+    }
+    if(queryParams.isCount()) {
+      Long countResult = elasticController.fileProvenanceByMLTypeCount(mlType.toString(),
+        mlAssetsParams.params(), queryParams.params());
+      return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK)
+        .entity(new SimpleResult<>(countResult)).build();
+    } else {
+      GenericEntity<List<MLAssetHit>> searchResults = new GenericEntity<List<MLAssetHit>>(
+        elasticController.fileProvenanceByMLType(mlType.toString(), mlAssetsParams.params(),
+          queryParams.params())) {
+      };
+      return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).entity(searchResults).build();
+    }
+  }
+}
