95716fe502bd5eb9d238216b490e627eb378f4b8
Robin Andersson <robin.eric.andersson@gmail.com>
2019-07-16 11:23:49 +0200

    fix

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/experiments/dto/ExperimentResultsDTO.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/experiments/dto/ExperimentResultsDTO.java
@@ -3,7 +3,6 @@ package io.hops.hopsworks.common.experiments.dto;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.List;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
